fd88a69368f082586556c4340c9eca7ffc3e0f87
Ilkin <ilkin.mammadovv@gmail.com>
2023-01-30 01:23:21 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -76,3 +76,18 @@ print(is_first_last_equal([10, 20, 30, 40, 10]))
 print(is_first_last_equal([10, 20, 30, 40, 50]))
 print(is_first_last_equal([]))
 
+def get_factorial(number):
+    factorial = 1
+    for x in range(1, number + 1):
+        factorial *= x
+    return factorial
+
+print(get_factorial(6))
+
+
+def get_factorial_recursive(number):
+    if number <= 1:
+        return 1
+    return number * get_factorial_recursive(number - 1)
+
+print(get_factorial_recursive(6))
