60a0c58b2471ce0d596e8bf741d9dd5f26144393
Ilkin <ilkin.mammadovv@gmail.com>
2023-01-30 14:22:33 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -93,5 +93,26 @@ def get_factorial_recursive(number):
 print(get_factorial_recursive(6))
 
 
+def get_day(user_info):
+    day = int(input('What is the day of your bday'))
+    user_info.append(day)
 
+def get_month(user_info):
+    month = int(input('What is the month(1-12) of your bday'))
+    user_info.append(month)
+    
+def get_year(user_info):
+    year = int(input('What is the year of your bday'))
+    user_info.append(year)
+    
+def get_user_bday(user_info):
+    get_day(user_info)
+    get_month(user_info)
+    get_year(user_info)
+    print('So your bday is', user_info)
+
+print('Hi, I will collect some info about your bday!')
+
+user_info = []
+get_user_bday(user_info)
 
